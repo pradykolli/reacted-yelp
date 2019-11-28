148a6a7b40398cea28464c47372b818c268e85bd
Pradeep Kolli <pradeepkolli3@yahoo.in>
2019-11-28 07:37:05 -0600

    added assets

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { TopNav } from "./TopNav/TopNav";
-
+import logo from "../../assets/logo.png";
+import styles from "./LandingPage.module.css"; 
 export function LandingPage(){
     return(
         <div>
             <TopNav/>
+            <img src={logo} alt="logo" className={styles.logo}></img>
        </div>
     );
 }
